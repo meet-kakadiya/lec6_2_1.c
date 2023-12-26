b733e76bf1e3aec3e32376f0f84159e6fb23a6e9
meetkakadiya <meetkakadiya308@gmail.com>
2023-12-26 16:56:55 +0530

    lec6_2_1.c

---
+++ lec_6_2.c
@@ -0,0 +1,25 @@
+  /* 
+Enter a number (n): 5
+Numbers from 1 to 5 are:
+1 2 3 4 5
+    */
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter a number (n): ");
+    scanf("%d", &n);
+
+    i = 1; 
+
+    printf("Numbers from 1 to %d are: \n", n);
+
+    
+    while (i <= n) {
+        printf("%d ", i);
+        i++; 
+    }
+
+    return 0;
+}
---
+++ lec_6_2_1.c
@@ -0,0 +1,23 @@
+/* 
+Enter a number (n): 5
+Numbers from 5 to 1 are:
+5
+
+    */
+
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number (n): ");
+    scanf("%d", &n);
+
+    printf("Numbers from %d to 1 are: \n", n);
+
+   
+    while (n >= 1) {
+        printf("%d ", n);
+        n--; 
+
+    }
